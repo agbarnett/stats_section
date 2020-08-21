66cfae5ddaf460583e3a4f5b5b5aadb384bf49c4
Nicole White <nm.white@qut.edu.au>
2020-08-21 10:49:46 +1000

    script to find most common words

---
+++ code/5_find_stopwords.R
@@ -0,0 +1,17 @@
+#5_find_stopwords.R
+library(tidyverse)
+library(stringr)
+library(stringi)
+
+load('./data/stats_section_info.rda')
+
+stats_section = bind_rows(stats_section)
+
+#change to native encoding
+stats_section = stats_section %>% mutate(text_data_clean = enc2native(text_data))
+#remove unicodes
+stats_section = stats_section %>% mutate(text_data_clean = str_remove_all(text_data_clean,
+                                                                          pattern="\\s*<U\\+\\w+>\\s*|\\<\\S\\S\\S\\S\\S\\S\\>"))
+#unnest to individual words
+all_words = stats_section %>% unnest(text_data_clean) %>% 
+  mutate(y=strsplit(text_data_clean,' ')) %>% pull(y) %>% unlist()

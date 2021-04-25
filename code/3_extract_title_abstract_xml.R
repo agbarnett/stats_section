ef175ad84ead96630e01ed639dc03987f2187918
Nicole White <nm.white@qut.edu.au>
2021-04-25 10:38:23 +1000

    amend xml script to extract subject classifications

--- code/3_extract_title_abstract_xml.R
+++ code/3_extract_title_abstract_xml.R
@@ -4,14 +4,15 @@ library(rplos)
 library(XML)
 library(tidyverse)
 
-#plos full text - e,g, batch 1
-batch.number = 1
-meta_dat = readRDS(paste0('./data/plos_meta_',batch.number,'.RDS'))
-
-doi_list = meta_dat %>% pull(doi)
+#plos full text - loop over batches
 dat = list()
+for (batch.number in 1:5){
+meta_dat = readRDS(paste0('./data/plos_meta_',batch.number,'.RDS'))
+doi_list = meta_dat %>% pull(doi)
 
-for(i in 1:100){
+dat.batch = list()
+
+for(i in 1:10){ #length(doi_list)
   #extract xml of full text. if no xml available, skip to the next doi
   full_text <- tryCatch(
     plos_fulltext(doi_list[i]),
@@ -21,11 +22,17 @@ for(i in 1:100){
   if(inherits(full_text, "error")) next
 
   full_text_xml <- xmlParse(full_text,asText=T,useInternalNodes = T,encoding='UTF-8')
-  methods_text = list()
+  
+  #title and abstract
   title_abstract = list()
   node_info.title = getNodeSet(full_text_xml,"//front//title-group//article-title")
   node_info.abstract = getNodeSet(full_text_xml,"//front//abstract")
   
+  #subject classifications
+  subjects = list()
+  node_info.subjects = getNodeSet(full_text_xml,"//front//article-categories/subj-group")
+    
+    
   title_abstract[['title']] = xmlValue(node_info.title)
   #first try extracting abstract minus section headings (eg aim, methods)
   title_abstract[['abstract']] = paste(unlist(as.character(sapply(node_info.abstract,xpathSApply,"./sec/p",xmlValue))),collapse=' ')
@@ -34,11 +41,28 @@ for(i in 1:100){
     title_abstract[['abstract']] = xmlValue(node_info.abstract)
   }
   
-  
+  #level 1 classification (e..g biology and life sciences)
+  #use as.characters() to force blank cells
+  subjects[['level1']] = unlist(as.character(sapply(node_info.subjects,xpathSApply,"./subject",xmlValue)))
+  subjects[['level2']] = unlist(as.character(sapply(node_info.subjects,xpathSApply,"./subj-group/subject",xmlValue)))
+  subjects[['level3']] = unlist(as.character(sapply(node_info.subjects,xpathSApply,"./subj-group/subj-group/subject",xmlValue)))
+    
   if (!is.null(title_abstract[['title']]) & !is.null(title_abstract[['abstract']])){
-    dat[[i]] = bind_cols(title_abstract) %>% add_column(doi=doi_list[i]) %>% mutate_if(is.factor,as.character)
-
+    dat.batch[[i]] = bind_cols(title_abstract) %>% add_column(doi=doi_list[i],.before = 1) %>% mutate_if(is.factor,as.character)
   }
+  
+  if (!is.null(subjects[['level1']])){ #must have level 1 subject classification at a minimum
+    dat.batch_subjects[[i]] = bind_cols(subjects) %>% distinct() %>% 
+      arrange(level1,level2,level3) %>% 
+      mutate_at(c("level1","level2","level3"),function(z) str_remove_all(z,'list\\(\\)')) %>% 
+      add_column(doi=doi_list[i],.before = 1) %>% mutate_if(is.factor,as.character)
+  }
+  
 }
+
+dat.title.abstract[[batch.number]] = bind_rows(dat.batch)
+dat.subject.class[[batch.number]] = bind_rows(dat.batch_subjects)
+}
+
 dat = bind_rows(dat)
-save(dat,file=paste0('./data/title_abstract_batch_',batch.number,'.rda'))
+save(dat,file='./data/title_abstract_plos.rda')
